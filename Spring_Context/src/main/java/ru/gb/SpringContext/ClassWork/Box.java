320a6db37a8cbdcb8c9ab509ec1a7273cae47f1a
Alexander <100999225+AlexanderNaide@users.noreply.github.com>
2023-01-19 12:02:19 +0300

    Context

---
+++ Spring_Context/src/main/java/ru/gb/SpringContext/ClassWork/Box.java
@@ -0,0 +1,26 @@
+package ru.gb.SpringContext.ClassWork;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@Scope("prototype")
+//@Scope("singleton")
+public class Box {
+    private String color;
+
+    @PostConstruct
+    public void init(){
+        this.color = "red";
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
