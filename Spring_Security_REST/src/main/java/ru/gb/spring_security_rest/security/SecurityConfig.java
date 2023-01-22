93ebe2c0b047a7001bfb546f7fcb38840b9d514c
Alexander <100999225+AlexanderNaide@users.noreply.github.com>
2023-01-22 22:02:33 +0300

    security_rest - start

---
+++ Spring_Security_REST/src/main/java/ru/gb/spring_security_rest/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package ru.gb.spring_security_rest.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.gb.spring_security_rest.api.MainFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(MainFilter filter, HttpSecurity http) throws Exception{
+        return http.
+                authorizeHttpRequests().
+                requestMatchers("/api/v1/products/cart").authenticated().
+                anyRequest().permitAll().
+//                and().formLogin().
+                and().addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class).
+                build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        // Это в развернутом виде, а ниже тоже самое, свернутое в лямбду
+//        return new WebSecurityCustomizer() {
+//            @Override
+//            public void customize(WebSecurity web) {
+//                web.ignoring().requestMatchers("/auth/**");
+//            }
+//        };
+
+        return webSecurity -> webSecurity.ignoring().requestMatchers("/auth/**");
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.builder()
+                .username("user")
+                .password("pass")
+                .authorities("ADMIN", "MANAGER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationProvider... providers){
+        return new ProviderManager(providers);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        return new StandardAuthenticationProvider();
+    }
+}
